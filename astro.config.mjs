f46627c6c5332afddf32d6613c11da64859e87bb
Wess-ctrl <wmdiscordpersonal001@gmail.com>
2026-09-02 23:07:50 +0200

    Despliegue a github2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Wess-ctrl.github.io', // Cambia 'tu-usuario' por tu usuario real de GitHub
+  base: '/Gove-art',    // Cambia por el nombre exacto de tu repositorio en GitHub
+});
